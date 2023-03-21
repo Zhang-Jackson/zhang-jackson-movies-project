90493699b7aa46ef4d5b00c2458a15a1151ebc86
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-03-21 10:25:59 -0700

    movie cards

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
 const dbUrl = "https://productive-bristle-edam.glitch.me/movies"
+const searchUrl ="https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher"
 
-$.get(dbUrl,{
+// $.get(dbUrl,{
+//
+// }).done(function(data){
+//     console.log(data);
+//     let buildHtml = "";
+//     data.forEach(function (data){
+//         buildHtml += '<p>' + data.title +
+//                     data.rating +
+//                     data.director +
+//             data.genre + '</p>'
+//     })
+//     $('#movieList').append(buildHtml);
+// });
 
-}).done(function(data){
-    console.log(data);
-});
+fetch(dbUrl).then(resp => resp.json())
+    .then(data => {
+        let html = '';
+        for(let i=0; i <data.length; i++) {
+            html += `<div class="card">`
+            html += `<div class="card-header">${data[i].title}</div>`
+            html += `<div class="card-body">`
+            html += `${data[i].director}`
+            html += `${data[i].rating}`
+            html += `${data[i].genre}`
+            html += `</div>`//end of body
+            html += `<div class="card-footer">`
+            html += `<button class="btn btn-primary">Edit</button>`
+            html += `</div>`//end of footer
+            html += `</div>`//end of card
+        }
+        $('#movieList').append(html);
+    })
