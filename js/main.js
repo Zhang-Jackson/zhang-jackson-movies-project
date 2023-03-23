015b25fdff6f0ac3ccca1de450e951fa1aa16c26
GageJackson <122929065+GageJackson@users.noreply.github.com>
2023-03-22 22:24:48 -0500

    all desired functionality done. design and refactoring now

--- js/main.js
+++ js/main.js
@@ -10,49 +10,47 @@ function getMovieListDbData() {
         });
 }
 function sortByRatings(){
-    console.log(selectSortType.value);
     switch (selectSortType.value){
         case '1': //rating low to high
             userMovieList.sort(
                 (a, b) => (parseInt(a.dbRating) > parseInt(b.dbRating)) ? 1 : (parseInt(a.dbRating) < parseInt(b.dbRating) ? -1 : 0)
             );
-            sortMe();
+            sortMe(userMovieList);
             break;
         case '2': //rating high to low
-            console.log(userMovieList);
             userMovieList.sort(
                 (a, b) => (parseInt(a.dbRating) < parseInt(b.dbRating)) ? 1 : (parseInt(a.dbRating) > parseInt(b.dbRating) ? -1 : 0)
             );
-            console.log(userMovieList);
-            sortMe();
+            sortMe(userMovieList);
             break;
         case '3': // sort alpa a-Z
             userMovieList.sort(
                 (a, b) => ((a.title) > (b.title)) ? 1 : ((a.title) < (b.title) ? -1 : 0)
             );
-            sortMe();
+            sortMe(userMovieList);
             break;
         case '4': //sort alpha z-a
             userMovieList.sort(
                 (a, b) => ((a.title) < (b.title)) ? 1 : ((a.title) > (b.title) ? -1 : 0)
             );
-            sortMe();
+            sortMe(userMovieList);
             break;
         default: //sort by id low to high
             userMovieList.sort(
                 (a, b) => (parseInt(a.id) > parseInt(b.id)) ? 1 : (parseInt(a.id) < parseInt(b.id) ? -1 : 0)
             );
-            sortMe();
+            sortMe(userMovieList);
             break;
     }
-
 }
 
-function sortMe(){
+function sortMe(movieSet){
     emptyButtonArrays();
     let html = "";
-    html = createMovieListHtml(userMovieList);
+    html = createMovieListHtml(movieSet);
     $('#movieList').html(html);
+
+    setGenreSelector();
 
     $('.movieTitle').each(function() {
         const titleLength = $(this).text().length;
@@ -61,7 +59,6 @@ function sortMe(){
         }
     });
 
-    console.log(`submit buttons: ${editSubmitBtns}`);
     createButtons(editSubmitBtns, editMovie);
     createButtons(editDeleteBtns, deleteMovie);
     createButtons(cardList, cardThing);
@@ -71,9 +68,7 @@ function createMovieListArray(data){
     for(let movie of data){
         userMovieList.push(makeMovieObject2(movie));
     }
-    sortMe();
-    console.log("called");
-    console.log(userMovieList);
+    sortMe(userMovieList);
 }
 
 function makeMovieObject2(data){
@@ -138,15 +133,6 @@ function addMovie(movieId, userRating){
     ).done(function (data) {
         console.log(data);
 
-        /*let newMovie = {
-            title:data.title,
-            rating:userRating,
-            poster:data.poster_path,
-            dbRating:data.vote_average,
-            dbId:data.id
-        }
-         */
-
         let newMovie = makeMovieObject(data);
 
         if(userRating >= 1 && userRating <= 10){
@@ -194,20 +180,24 @@ function selectMovie(e){
 
     let userRating = addRating.value;
 
-    console.log(`Db id: ${dbId}`);
     addMovie(dbId, userRating);
     movieSearchDiv.innerHTML = "";
 }
 
 function cardThing(e){
-    let btn = (e.target.id).indexOf("-");
-    let btnId = (e.target.id).slice(btn + 1);
+    let back = (e.target.parentNode.parentNode.id).indexOf("-");
+    let backId = (e.target.parentNode.parentNode.id).slice(back + 1);
+    let back2 = (e.target.id).indexOf("-");
+    let backId2 = (e.target.id).slice(back2 + 1);
 
+    let clickedBackText = document.getElementById(`cardBack-${backId}`);
+    let clickedBackText2 = document.getElementById(`cardBack-${backId2}`);
 
-    let clickedItem = document.getElementById(`card-${btnId}`)
-    console.log(clickedItem);
-
-    //let dbId = db.innerText;
+    if (clickedBackText){
+        clickedBackText.hidden = !clickedBackText.hidden;
+    } else {
+        clickedBackText2.hidden = !clickedBackText2.hidden;
+    }
 }
 
 function editMovie(e){
@@ -220,8 +210,6 @@ function editMovie(e){
     let db = document.getElementById(`editMovieDbId-${btnId}`)
     let dbId = db.innerText;
 
-    console.log("db ID:");
-    console.log(dbId);
     getMovieObjectData(movieId, dbId, btnId);
 }
 
@@ -230,7 +218,6 @@ function getMovieObjectData(movieId, dbId, btnId){
         `https://api.themoviedb.org/3/movie/${dbId}?api_key=${movieKey}`,
         {}
     ).done(function (data) {
-        console.log(data);
         let movieObject = makeMovieObject(data);
 
         let editRating = document.getElementById(`editMovieRating${btnId}`);
@@ -249,7 +236,6 @@ function getMovieObjectData(movieId, dbId, btnId){
 function makeMovieObject(data){
     let genreArray = [];
     for(let genre of data.genres){
-        console.log(`Genre: ${genre.name}`);
         genreArray.push(genre.name);
     }
     return {
@@ -295,30 +281,76 @@ function hideLoading() {
     },1500)
 }
 
+function createGenreHtml(data){
+    let html = "";
+    for(let genre of data){
+        html += `<div class="badge bg-primary col-auto">`
+        html += `<span>${genre}</span>`
+        html += `</div>`
+
+        if(!genreList.includes(genre)){
+            genreList.push(genre);
+        }
+    }
+    return html;
+}
+
+function setGenreSelector(){
+    let html =``;
+    html += `<option selected value="0">select genre</option>`
+    for(let genre of genreList){
+        html += `<option value="${genre}">${genre}</option>`
+    }
+    selectGenre.innerHTML = html;
+}
+
+function sortByGenre(){
+    moviesByGenre = [];
+    let selectedGenre = selectGenre.value;
+    for(let movie of userMovieList){
+        let isMatch = false;
+        for(let genre of movie.genres){
+            if(selectedGenre === genre){
+                isMatch = true;
+            }
+        }
+        if(isMatch){
+            moviesByGenre.push(movie);
+        }
+    }
+    sortMe(moviesByGenre);
+}
+
 function createMovieListHtml(data){
     let html = '';
     for(let i = 0; i < data.length; i++) {
         let databaseRating = data[i].dbRating;
-        html += `<div class="card col-auto px-0" id="card-${[i]}">`;
+        html += `<div class="card col-auto px-0" id="cardFront-${[i]}" data-front-hidden="false">`;
         html += `<div class="card-header p-0  img-fluid position-relative">`;
         html += `<span class=" avg-badge badge rounded-pill">${parseFloat(databaseRating).toFixed(1)}</span>`;
         html += `<span class=" usr-badge badge rounded-pill">${data[i].rating}</span>`;
-        html += `<img src="https://image.tmdb.org/t/p/w300/${data[i].poster}" alt="movie" class="rounded-top img-fluid" id="image-${[i]}">`;
+        html += `<img src="https://image.tmdb.org/t/p/w300/${data[i].poster}" alt="movie" class="rounded img-fluid" id="image-${[i]}">`;
         html += `<button type="button" class="btn btn-primary rounded-circle btn-edit p-2" id="btn-edit${[i]}" data-bs-toggle="modal" data-bs-target="#editModal${i}">`;
         html += `<img src="/assets/pencil.svg" alt="edit icon" style="width: 18px; aspect-ratio: 1;"></button>`;
         html += `</div>`;
 
-        html += `<div class="card-body pt-0 border">`;
-        html += `<p class="movieTitle text-center">${data[i].title}</p>`;
-        //html += `<p><span>Genre(s): </span>${data[i].genre}</p>`
-        html += `</div>`;
-        html += `</div>`;//end of card
 
+
+        html += `<div class="card border overflow-scroll position-absolute p-2" data-back-hidden="true" id="cardBack-${[i]}" hidden="hidden">`;
+        html += `<p class="movieTitle text-center notSelectable">${data[i].title}</p>`
+
+        html += `<div class="row mx-auto d-flex justify-content-center gap-1 mb-3">`
+        html += createGenreHtml(data[i].genres);
+        html += `</div>`
+
+        html += `<p class="notSelectable">${data[i].overview}</p>`
+        html += `<p class="notSelectable">Release: ${data[i].release}`;
+        html += `<p class="notSelectable" >Runtime: ${data[i].runtime} minutes</p>`
+        html += `</div>`;//end card2
+        html += `</div>`;//end of card set
         html += createModalHtml(data[i], i);
-        cardList.push(`#card-${[i]}`);
+        cardList.push(`#cardFront-${[i]}`);
     }
-
-    console.log(cardList);
     return html;
 }
 
@@ -401,16 +433,20 @@ let userMovieList = [];
 let editDeleteBtns = [];
 let editSubmitBtns = [];
 let selectMovieBtns = [];
+let genreList = [];
 let cardList = [];
+let moviesByGenre = [];
 
 let addTitle = document.getElementById("addMovieName");
 let addRating = document.getElementById('addMovieRating');
 const movieSearchDiv = document.getElementById("movieSearchDiv");
 const searchMovieBtn = document.querySelector("#btn-addMovie");
-const selectSortType = document.querySelector("#select-sort")
+const selectSortType = document.querySelector("#select-sort");
+const selectGenre = document.querySelector("#select-genre");
 
 searchMovieBtn.addEventListener("click", searchMovie);
 selectSortType.addEventListener("change", sortByRatings);
+selectGenre.addEventListener("change", sortByGenre);
 
 getMovieListDbData();
 
